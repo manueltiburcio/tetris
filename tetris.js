926f14ae61a329ed26a9af04f2941decd316c3d3
Manuel Tiburcio <manuelmtiburcio@gmail.com>
2021-12-29 12:28:00 -0400

    Added Fast Drop function

--- tetris.js
+++ tetris.js
@@ -11,6 +11,20 @@ const matrix = [
     [0, 1, 0]
 ];
 
+function collide(arena, player) {
+    const [m, o] = [player.matrix, player.pos];
+    for (let y = 0; y < m.length; ++y){
+        for (let x = 0; x < m[y].length; ++x){
+            if (m[y][x] !== 0 && 
+                (arena[y + o.y] &&
+                arena[y + o.y][x + o.x]) !== 0){
+                    return true;
+                }
+        }
+    }
+    return false;
+}
+
 function createMatrix(w, h){
     const matrix = [];
     while (h--){
@@ -22,7 +36,7 @@ function createMatrix(w, h){
 function draw() {
     context.fillStyle = '#000';
     context.fillRect(0,0, canvas.width, canvas.height);
-
+    drawMatrix(arena, {x: 0, y: 0});
     drawMatrix(player.matrix, player.pos );
 }
 
@@ -41,13 +55,74 @@ function drawMatrix(matrix, offset){
 
 function merge(arena, player){
     player.matrix.forEach((row, y) => {
-        
-    })
+        row.forEach((value, x) => {
+            if (value !== 0) {
+                arena[y + player.pos.y][x + player.pos.x] = value;
+            }
+        });
+    });
 }
 
 function playerDrop(){
     player.pos.y++;
+    if(collide(arena, player)){
+        player.pos.y--;
+        merge(arena, player);
+        player.pos.y = 0;
+    }
     dropCounter = 0;
+}
+
+function playerFastDrop(){
+    do {
+        if(collide(arena, player)){
+            player.pos.y--;
+            merge(arena, player);
+            player.pos.y = 0;
+        }
+        dropCounter = 0;
+    } while (player.pos.y++)
+}
+
+function playerRotate(dir){
+    const pos = player.pos.x;
+    let offset = 1;
+    rotate(player.matrix, dir);
+    while (collide(arena, player)){
+        player.pos.x += offset;
+        offset = -(offset + (offset > 0 ? 1 : -1));
+        if (offset > player.matrix[0].length) {
+            rotate(player.matrix, -dir);
+            player.pos.x = pos;
+            return;
+        }
+    }
+}
+
+function playerMove(dir){
+    player.pos.x += dir;
+    if(collide(arena,player)){
+        player.pos.x -= dir;
+    }
+}
+
+function rotate(matrix, dir) {
+    for (let y = 0; y < matrix.length; ++y){
+        for (let x = 0; x < y; ++x){
+            [
+                matrix [x][y],
+                matrix [y][x],] = 
+                [ 
+                    matrix [y][x],
+                    matrix [x][y],
+                ];
+        }
+    }
+    if (dir > 0) {
+     matrix.forEach(row => row.reverse());   
+    } else {
+        matrix.reverse();
+    }
 }
 
 let dropCounter = 0;
@@ -77,12 +152,18 @@ const player = {
 
 document.addEventListener('keydown', event => {
     if (event.keyCode == 37){
-        player.pos.x--;
+        playerMove(-1);
     } else if (event.keyCode === 39){
-        player.pos.x++;
-    } else if (event.keyCode === 40){
+        playerMove(+1);
+    } else if (event.keyCode === 38){
+        playerFastDrop();
+    }else if (event.keyCode === 40){
         playerDrop();
+    } else if (event.keyCode === 81){
+        playerRotate(-1);
+    } else if (event.keyCode === 87){
+        playerRotate(1);
     }
-})
+});
 
 update();
